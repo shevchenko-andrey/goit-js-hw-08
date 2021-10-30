690e00e54513e613ce0a605cea337d322eb834ee
shevchenko-andrey <andreases_mobimoz@ukr.net>
2021-10-31 00:57:48 +0300

    edd feature 03 feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const feedbackForm = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const feedbackFormState = {};
+populateFormInput();
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onSaveProgressInput, 500));
+function onSaveProgressInput(e) {
+  let feedbackFormState = localStorage.getItem(LOCALSTORAGE_KEY);
+  feedbackFormState = feedbackFormState ? JSON.parse(feedbackFormState) : {};
+  feedbackFormState[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormState));
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
 
+function populateFormInput(e) {
+  let savedInputForm = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedInputForm) {
+    savedInputForm = JSON.parse(savedInputForm);
+    Object.entries(savedInputForm).forEach(([name, value]) => {
+      feedbackForm.elements[name].value = value;
+    });
+  }
+}
