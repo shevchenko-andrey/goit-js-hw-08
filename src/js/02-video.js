9dd8d554e99437a65ad9a09026d60c8fbc9e3ea8
shevchenko-andrey <andreases_mobimoz@ukr.net>
2021-10-30 19:25:33 +0300

    add feature 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const onSaveProgressPlayer = e => {
+  localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+};
+player.on('timeupdate', throttle(onSaveProgressPlayer, 1000));
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        currentTime = 0;
+        break;
 
+      default:
+        currentTime = 0;
+        break;
+    }
+  });
+}
